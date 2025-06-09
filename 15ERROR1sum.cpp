e57c6330e26585b4844178b019bfcf0aacc70ac9
anandadhar087 <134637749+anandadhar087@users.noreply.github.com>
2025-06-10 00:10:26 +0600

    project added

---
+++ 15ERROR1sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x, y;
+cout << "Type a number: ";
+cin >> x;
+cout << "Type another number: ";
+cin >> y;
+cout << "Sum is: " << x+y << endl;
+cout << "Division is: " << (float) x/y ;
+return 0;
+}
